9f4aa71ac49174d317813a85788e85f7850e073f
kostas <kostasder94x@gmail.com>
2017-07-16 01:25:31 +0300

    Villages fixed

--- ask2a/src/ask2a/Villages.java
+++ ask2a/src/ask2a/Villages.java
@@ -23,28 +23,38 @@ public class Villages
  
         // Initialize all subsets as single element sets
         for (int i=0; i<this.N; ++i){
-            parent[i]=-1;
+            parent[i]=i;
         }
     }
  
-    // A utility function to find the subset of an element i
     int find(int i){
-	if (DEBUG)
-	     	System.out.println(i);
-    	while (parent[i] != -1){
+    	while (parent[i] != i){	
     		parent[i]=parent[parent[i]];
-    		i=parent[i];
+    		i=parent[i];		
         }
         return i;
     }
  
-    // A utility function to do union of two subsets
     void Union(int x, int y){
+      try{
     	int xset = find(x);
         int yset = find(y);
    		parent[xset] = yset;
-   		
-   		this.sets=this.sets-1;
+   		if(xset!=yset)
+	   		this.sets=this.sets-1;
+	}
+      catch(java.lang.ArrayIndexOutOfBoundsException e){
+	      e.printStackTrace();
+	      System.out.println(x+" "+y);
+		System.exit(1);
+      }    	
+      if (DEBUG){
+		System.out.println("Union"+"("+this.sets+")"+x+" "+y);
+		for (Integer p:parent)
+			System.out.print(p+",");
+		System.out.println("nl");
+	}
+
     }
     
     public static void main(String [] args) throws IOException{
@@ -59,14 +69,14 @@ public class Villages
     	int K=new Integer (el[2]);
     	
     	Villages graph = new Villages(N, M);
-    	
+    	if (DEBUG)
+    		System.out.println(N+" "+M+" "+K);
+
     	for(int i=0; i<M; i++){
     		line = in.readLine();
     		el=line.split(" ");
     		int x=new Integer (el[0]) -1;
     		int y=new Integer (el[1]) -1;
-		if (DEBUG)
-    			System.out.println(x+" "+y);
     	    graph.Union(x, y);
     	}
     	int result=graph.sets-K;
